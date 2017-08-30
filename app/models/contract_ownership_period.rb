4a1802c2fa48d421ad727fd796f7f0873ce08b12
Benjamin Blue <bblue@Benjamins-MacBook-Pro.local>
2017-08-30 16:46:02 -0600

    Add contract associations

--- app/models/contract_ownership_period.rb
+++ app/models/contract_ownership_period.rb
@@ -1,2 +1,5 @@
 class ContractOwnershipPeriod < ApplicationRecord
+  belongs_to :owner
+  belongs_to :contract
 end
+
--- app/models/contract_year.rb
+++ app/models/contract_year.rb
@@ -1,2 +1,4 @@
 class ContractYear < ApplicationRecord
+  belongs_to :owner
+  belongs_to :contract
 end
