d96c039e2a76d08072bc190c83c74d3c8e32067c
Benjamin Blue <bblue@Benjamins-MacBook-Pro.local>
2017-08-30 16:46:22 -0600

    Update players view

--- app/controllers/players_controller.rb
+++ app/controllers/players_controller.rb
@@ -1,6 +1,6 @@
 class PlayersController < ApplicationController
 
   def index
-    @printable_player = Player.pluck(:id, :name, :position, :team)
+    @printable_player = Player.includes(:team).pluck(:id, :name, :position, "teams.name")
   end
 end
